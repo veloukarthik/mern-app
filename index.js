9250dbe903308122b6cc31e68858d66bf6be7c75
veloukarthik <71749429+veloukarthik@users.noreply.github.com>
2023-03-28 05:11:03 +0000

    node js inital setup is completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("testing")
+const express = require("express");
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("I am running via github workspace")
+})
+
+
+app.listen(5000,(()=>{
+    console.log("server is running 5000 port number");
+}))
